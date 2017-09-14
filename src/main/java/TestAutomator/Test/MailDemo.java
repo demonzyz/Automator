3f891ef7f20a58c00cea23a36236b81c7a251575
demonzyz <13204854@qq.com>
2017-09-14 16:18:46 +0800

    third commit

--- src/main/java/TestAutomator/Test/MailDemo.java
+++ src/main/java/TestAutomator/Test/MailDemo.java
@@ -26,8 +26,7 @@ public class MailDemo {
 		Session session = Session.getInstance(properties);
 		
 		//开启session的debug模式，这样就可以查看到程序发送Email的运行状态
-		
-		
+			
 		session.setDebug(true);
 		
 		//2、通过session得到transport对象
