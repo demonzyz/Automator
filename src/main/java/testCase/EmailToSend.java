219ae0fbb23b5a329391ab7c459e45ed6479d59c
demonzyz <13204854@qq.com>
2017-11-03 17:31:11 +0800

    sendMail and SimpleMail Finish

--- src/main/java/testCase/EmailToSend.java
+++ src/main/java/testCase/EmailToSend.java
@@ -1,5 +1,6 @@
 package testCase;
 
+import java.lang.reflect.Field;
 import java.util.Properties;
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
@@ -12,71 +13,61 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-
-
+import javax.mail.internet.MimeMultipart;
+import util.Parameter;
 
 public class EmailToSend {
 	
-	static String sendMail = "nnnnnn13@163.com";
-	
-	static String receiveMail = "nnnnnn13@163.com";
-	static String subject = "只包含文本的简单邮件【打折钜惠";
+	private String subject = null;
+	private MimeMessage mimeMsg; 		//MIME邮件对象   
+	private Session session; 			//邮件会话对象   
 	private Properties props;			//系统属性    
-	
-	private MimeMessage mimeMsg;
-	private Multipart mp; 
+	private Multipart mp; 				//Multipart对象,邮件内容,标题,附件等内容均添加到其中后再生成MimeMessage对象   
 	private String from = null;			//发件人邮箱地址
 	private String senderName = null;	//发件人显示名称
 	private String setTo = null;		//收件人邮箱地址，多个地址时以英文逗号分隔
 	private String copyTo = null;		//抄送人邮箱地址，多个地址时以英文逗号分隔
 	private String smtp = null;			//发件人SMTP服务器地址
 	private String filePath = null;		//附件的路径
+	private String userName = null;		//发件人邮箱用户名
+	private String password = null;		//发件人邮箱密码
 	
-	public static void main(String[] args)  throws Exception{
-		Properties properties = new Properties();
-		properties.setProperty("smtp", "smtp.163.com");
-		properties.setProperty("mail.transport.protocol", "smtp");	
-		properties.setProperty("mail.smtp.auth", "true");
-		
-		Session session = Session.getDefaultInstance(properties);
-		session.setDebug(true);
-		
-		MimeMessage message = createSimpleMail(session,sendMail,receiveMail);
-
-			Transport transport = session.getTransport();
-
-				transport.connect("smtp.163.com", "nnnnnn13", "q12345678");
-
-				transport.sendMessage(message, message.getAllRecipients());
-
-				transport.close();
+	/** 
+	* Constructor 
+	* @param parameters 邮件发送的配置参数
+	*/  
+	public EmailToSend(Parameter parameters){   
+		init(parameters);		
 	}
 	
-	private static MimeMessage createSimpleMail(Session session,String sendMail,String receiveMail) 
-	
-			throws Exception {
-		
-		//创建邮件对象
-		MimeMessage message = new MimeMessage(session);
-		
-		//创建邮件的发件人
-		
-		message.setFrom(new InternetAddress(sendMail));
-		
-		//指明邮件的收件人
-		message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiveMail));
-		
-		//邮件的标题
-		
-		message.setSubject("只包含文本的简单邮件【打折钜惠");
-		
-		//邮件的文本内容
-		
-		message.setContent("你好啊,XX用户你好, 今天全场5折, 快来抢购, 错过今天再等一年。。。", "text/html;charset=UTF-8");
-		
-		//返回创建好的邮件对象
-
-		return message;
+    @SuppressWarnings("null")
+	protected void init(Parameter parameters){
+		//检查是否缺少发送邮件的必要参数
+		String[] argNames= {"smtp", "from", "setTo", "userName", "password","subject"};
+		for(String name:argNames){
+			String value = parameters.get(name);
+			if(value==null || value.equals("")){
+				System.out.println("缺少发送邮件的必要参数:"+name);
+			}
+		}
+		//通过反射初始化参数列表
+		for(String name :parameters.asMap().keySet()){
+			String value = parameters.get(name);
+			if(value!=null || !value.equals("")){
+				try {
+					Field field = this.getClass().getDeclaredField(name);
+					field.set(this, value);				
+				} catch (NoSuchFieldException e) {
+					System.out.println("EmailSender不存在如下参数名："+name);
+				} catch (SecurityException e) {
+					System.out.println("初始化EmailSender 参数异常!");
+				} catch (IllegalArgumentException e) {
+					System.out.println("EmailSender 以下参数，参数值类型不正确："+name);
+				} catch (IllegalAccessException e) {
+					System.out.println("初始化EmailSender 参数异常!");
+				}			   
+			}
+		}	
 	}
 	
 	protected boolean setSubject() {
@@ -96,9 +87,9 @@ public class EmailToSend {
      * 邮件正文
      * @param String text,String encoding   
      */ 
-	protected boolean setBody(String text,String encoding) {
-		MimeBodyPart textBody = new MimeBodyPart();
+	protected boolean setBody(String text,String encoding) {		
 		try {
+			MimeBodyPart textBody = new MimeBodyPart();
 			textBody.setContent(text,encoding);
 			mp.addBodyPart(textBody);
 			return true;
@@ -123,31 +114,39 @@ public class EmailToSend {
             mp.addBodyPart(bp);   	              
             return true;   
         } catch(Exception e){  
-        	System.out.println("增加邮件附件："+filePath+"发生错误！");
+        		System.out.println("增加邮件附件："+filePath+"发生错误！");
             return false;   
         }   
     } 
 
 
-	
+    /**  
+     * 设置收件人
+     * @param 
+     */  
 	protected boolean setTo() {
 		try {
 			mimeMsg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(setTo));
+			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("收件人地址设置有误！");
-		} 
-		return false;
+			return false;
+		} 	
 	}
 	
+    /**  
+     * 设置发件人
+     * @param 
+     */  
   	protected boolean setFrom() {   
         try{   
-        	if(senderName ==null || senderName.equals("")){
+        	if(senderName == null || senderName.equals("")){
         		mimeMsg.setFrom(new InternetAddress(from)); 
         	}else mimeMsg.setFrom(new InternetAddress(from, senderName));
             return true;   
         } catch(Exception e) {
-        System.out.println("设置发信人异常 ！");
+        		System.out.println("设置发信人异常 ！");
             return false;   
         }   
     }  
@@ -159,12 +158,12 @@ public class EmailToSend {
   	protected boolean setCopyTo()   
     {    
         try{   
-        mimeMsg.setRecipients(Message.RecipientType.CC,InternetAddress.parse(copyTo));   
-        return true;   
+        		mimeMsg.setRecipients(Message.RecipientType.CC,InternetAddress.parse(copyTo));   
+        		return true;   
         }   
         catch(Exception e) {
-        System.out.println("设置抄送人 异常 ！ ");
-        	return false;
+        		System.out.println("设置抄送人 异常 ！ ");
+        		return false;
         } 
     } 
   	
@@ -183,13 +182,90 @@ public class EmailToSend {
      * @param need 
      */  
   	protected void setNeedAuth(boolean need) {   
-        if(props == null) props = System.getProperties();   
+        if(props == null) {
+         	props = System.getProperties(); 
+        }
         if(need){   
             props.put("mail.smtp.auth","true");   
         }else{   
             props.put("mail.smtp.auth","false");   
         }   
     }  	
-
   	
+    /** 
+     * 创建MIME对象 
+     * @param  
+     */  
+  	protected boolean createMimeMessage() {
+  		try {
+			session = Session.getDefaultInstance(props, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Session对象创建失败！");
+			return false;
+		}
+  		try {
+			mimeMsg = new MimeMessage(session);
+			mp = new MimeMultipart();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("message创建失败");
+			return false;
+		}
+    } 
+
+  	protected boolean sendOut(boolean needCopyTo) {
+  		try {
+  			mimeMsg.setContent(mp);
+			mimeMsg.saveChanges();
+			Session session = Session.getDefaultInstance(props);
+			session.setDebug(true);
+			Transport transport = session.getTransport("smtp");
+			transport.connect((String)props.getProperty("mail.smtp.host"), userName, password);
+			transport.sendMessage(mimeMsg, mimeMsg.getRecipients(Message.RecipientType.TO));
+			if (needCopyTo) {
+				transport.sendMessage(mimeMsg, mimeMsg.getRecipients(Message.RecipientType.CC));
+			}
+			transport.close();
+			return true;
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			System.out.println("邮件发送失败！");
+			return false;
+		}
+  		
+		
+	}
+  	
+	/** 
+     * 调用sendOut方法完成邮件发送
+     * @throws MessagingException 
+     */  
+    public boolean send(String text, String encoding){  
+    		boolean needCopyto = true;	   
+		setSmtpHost();   		//设置smtp服务地址
+	    createMimeMessage();  	//创建会话
+        setNeedAuth(true); 		//设置验证  
+        if(!setSubject()) return false;  	//设置主题
+        if(!setBody(text, encoding))return false;  		//设置正文
+        
+        if(!(filePath==null || filePath.equals(""))){		//设置附件
+        	if(!addFileAffix()) return false;   
+        }
+        
+        if(!setTo()) return false; 	//设置收件人
+        
+        if(copyTo==null || copyTo.equals("")){	//设置抄送人
+        		needCopyto = false;	        	
+        }else{
+        	if(!setCopyTo()) return false; 
+        }
+        
+        if(!setFrom()) return false;  	//设置发件人
+
+        if(!sendOut(needCopyto)) return false;  //调用发送函数       
+        
+        return true;             
+    } 	
 }
--- src/main/java/util/Parameter.java
+++ src/main/java/util/Parameter.java
@@ -9,61 +9,116 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import base.AppConfig;
 
 public class Parameter extends DesiredCapabilities{
- private final Map<String, Object> capabilities = new HashMap<>();
-
- public void setCapability(String capabilityName, String value) {
-  capabilities.put(capabilityName, value);
- }
- 
- String[] argNames= {"smtp", "from", "setTo", "userName", "password"};
-
- 
- public void setSmtp() {
-//  setCapability("mail.smtp.host", AppConfig.SMTPserver);
-  setCapability("mail.smtp.host", "smtp.163.com");
- }
- 
- public void setProtocol() {
-  setCapability("mail.transport.protocol", "smtp");
- }
- 
- public void setSmtpAuth(boolean need) {
-  if (need) {
-   setCapability("mail.smtp.auth", true);
-  }else {
-   setCapability("mail.smtp.auth", false);
-  }
-  
- }
- 
- public void setFrom(String from) {
-  setCapability("", from);
- }
- 
- public void setSetTo() {
-  setCapability("mail.smtp.auth", "true");
- }
- 
- public void setUserName() {
-  setCapability("mail.smtp.auth", "true");
- }
- 
- public void setPassWord() {
-  setCapability("mail.smtp.auth", "true");
- }
- 
- public String getSmtp() {
-  Object smtp = capabilities.get("mail.host");
-  return smtp == null ? "mail.hostÎª¿Õ":smtp.toString();
-  
- }
- 
- public static void main(String[] args) {
-  Parameter parameter = new Parameter();
-  parameter.setSmtp();
-  String value = parameter.getSmtp();
-  System.out.println(value);
- }
- 
-
+//	private final Map<String, Object> capabilities = new HashMap<>();
+//
+//	public void setCapability(String capabilityName, String value) {
+//		capabilities.put(capabilityName, value);
+//	}
+// 
+//	String[] argNames= {"smtp", "from", "setTo", "userName", "password"};
+//
+// 
+//	public void setSmtp() {
+//	//  setCapability("mail.smtp.host", AppConfig.SmtpServer);
+//		setCapability("mail.smtp.host", "smtp.163.com");
+//	}
+// 
+//	public void setProtocol() {
+//	//  setCapability("mail.smtp.host", AppConfig.SmtpProtocol);
+//		setCapability("mail.transport.protocol", "smtp");
+//	}
+//	
+//	public void setAuth() {
+//	//  setCapability("mail.smtp.host", AppConfig.SmtpAuth);
+//		setCapability("mail.smtp.auth", "true");
+//	}
+// 
+//	public void setSmtpAuth(boolean need) {
+//		if (need) {
+//			setCapability("mail.smtp.auth", true);
+//		}else {
+//			setCapability("mail.smtp.auth", false);
+//		}
+//  
+//	}
+// 
+//	public void setFrom(String from) {
+//		setCapability("", from);
+//	}
+// 
+//	public void setSetTo() {
+//		setCapability("mail.smtp.auth", "true");
+//	}
+// 
+//	public void setUserName() {
+//		setCapability("mail.smtp.auth", "true");
+//	}
+// 
+//	public void setPassWord() {
+//		setCapability("mail.smtp.auth", "true");
+//	}
+// 
+//	public String getSmtp() {
+//		Object smtp = capabilities.get("mail.smtp.host");
+//		return smtp == null ? "mail.hostÎª¿Õ":smtp.toString();
+//	}
+// 
+//	public String getProtocol() {
+//		Object smtp = capabilities.get("mail.transport.protocol");
+//		return smtp == null ? "mail.transport.protocolÎª¿Õ":smtp.toString();
+//	}
+//	
+//	public String getAuth() {
+//		Object smtp = capabilities.get("mail.smtp.auth");
+//		return smtp == "true" ? "true":"fasle";
+//	}
+//	
+//	public String get(String name){
+//		Object cap = getCapability(name);	
+//		if(cap==null){
+//			return null;
+//		}else return cap.toString();	
+//	}
+// 
+//	public static void main(String[] args) {
+//		Parameter parameter = new Parameter();
+//		parameter.setSmtp();
+//		parameter.setProtocol();
+//		parameter.setAuth();
+//		String value = parameter.getSmtp();
+//		String vString	= parameter.getProtocol();
+//		String value1 = parameter.getAuth();
+//		System.out.println(value);
+//		System.out.println(vString);
+//		System.out.println(value1);
+//	}
+// 
+	
+	public void set(String name, String value){
+		setCapability(name, value);
+	}
+	
+	public void set(String name, boolean value){
+		setCapability(name, value);
+	}
+	
+	public void set(String name, Object value){
+		setCapability(name, value);
+	}
+	
+	public String get(String name){
+		Object cap = getCapability(name);	
+		if(cap==null){
+			return null;
+		}else return cap.toString();	
+	}
+	
+	public boolean isExist(String name){
+		Object cap = getCapability(name);
+	    if (cap == null) {
+	        return false;
+	    }else {
+	    	return true;  
+	    }		
+	}
 }
